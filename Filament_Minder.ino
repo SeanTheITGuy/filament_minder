12c36d8cab22400d42b837481d2f880e635a4c69
SeanTheITGuy <seanallanmurphy@gmail.com>
2019-06-28 14:31:34 -0300

    Updated to work...

--- Filament_Minder.ino
+++ Filament_Minder.ino
@@ -11,38 +11,33 @@
 
 #include <HX711.h>
 #include <EEPROM.h>
-#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
-#include <splash.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SPITFT.h>
-#include <Adafruit_SPITFT_Macros.h>
-#include <gfxfont.h>
 
 #define TITLE "Filament Minder"
-#define VERSION "v0.4.1"
+#define VERSION "v0.5.0"
 #define SPOOLCOUNT 8              // Number of spools we wish to store. Can be up to [ (EEPROM.length()/sizeof(spool)) - sizeof(spool) ] 
-#define PREVPIN 5                 // pin that the "prev spool" button is on
-#define NEXTPIN 4                 // pin that the "next spool" button is on
+#define PREVPIN 4                 // pin that the "prev spool" button is on
+#define NEXTPIN 5                 // pin that the "next spool" button is on
 #define RESETPIN 6                // pin that the "new spool" button is on
 #define BUZZERPIN 7               // screecher pin
 #define THRESHOLD 50              // minimum amount (g) of remaining filament before alert.
 #define TIMEBETWEENBEEPS 20000    // milliseconds between beep alerts
-#define LOADCELL_DOUT_PIN 2       // DOUT pin for HX711 load cell ADC
-#define LOADCELL_SCK_PIN 3        // SCK pin of HX711 load cell ADC
-#define LOADCELL_OFFSET 50682624  // Define these after calibration.
-#define LOADCELL_DIVIDER 5895655  // Define these after calibration.
+#define LOADCELL_DOUT_PIN 3       // DOUT pin for HX711 load cell ADC
+#define LOADCELL_SCK_PIN 2        // SCK pin of HX711 load cell ADC
+#define LOADCELL_OFFSET 145700    // Define these after calibration.
+#define LOADCELL_DIVIDER -172.5   // Define these after calibration.
 #define SCREEN_WIDTH 128          // OLED display width, in pixels
 #define SCREEN_HEIGHT 64          // OLED display height, in pixels
 #define OLED_RESET -1             // Reset pin # (or -1 if sharing Arduino reset pin)
 #define OLED_ADDR 0x3C            // I2C address of the OLED panel.
 
-//#define DEBUG
+#define DEBUG
 #ifdef DEBUG
-  #define DEBUG_START(x) DEBUG_START(x); 
-  #define DEBUG_PRINT(x) DEBUG_PRINT(x)
-  #define DEBUG_PRINTLN(x) DEBUG_PRINTLN(x)
+  #define DEBUG_START(x) Serial.begin(x)
+  #define DEBUG_PRINT(x) Serial.print(x)
+  #define DEBUG_PRINTLN(x) Serial.println(x)
 #else
   #define DEBUG_START(x)
   #define DEBUG_PRINT(x)
@@ -74,14 +69,12 @@ void setup() {
   DEBUG_START(115200);
 
   // Set up the display
-  if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) { 
-    DEBUG_PRINTLN(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  
   // Display stored logo buffer TODO: replace the logo.
   display.display();
   delay(1000); // pause for 1 second.
+  
   
   // Configure the load cell
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
@@ -119,15 +112,13 @@ void setup() {
 
 void loop() {  
 
-  // Get the current weight from the loadcell (retry 100 times, then error out). get_units(X) averages X measurements.
-  if (scale.wait_ready_retry(100)) {
-    currentWeight = (int) scale.get_units(10);    // cast the long float response to an int for 1g granularity
-    if (currentWeight < 0) {
-      currentWeight = 0;                          // if measured data not valid, init to 0
-    }
-  } else {
-      error("SCALE ERR", 0);       // if load cell ADC not ready after 100 retries, display error.
- }
+  // Get the current weight from the loadcell. get_units(X) averages X measurements.
+  currentWeight = (int) scale.get_units(5);    // cast the long float response to an int for 1g granularity
+  DEBUG_PRINTLN(currentWeight);
+  if (currentWeight < 0) {
+    currentWeight = 0;                          // if measured data not valid, init to 0
+  }
+  
 
   // Check to see if a spool button is pressed to switch between spools
   int buttons[] = {PREVPIN, NEXTPIN};
@@ -146,7 +137,7 @@ void loop() {
       } else {
         id = id + 1 >= SPOOLCOUNT ? SPOOLCOUNT - 1 : id + 1;
       }
-      EEPROM.put(sizeof(spool)*SPOOLCOUNT, id);
+      EEPROM.update(sizeof(spool)*SPOOLCOUNT, id);
       
       // Retrieve the new spool's data
       EEPROM.get(sizeof(struct spool)*id, currentSpool);  
@@ -163,7 +154,6 @@ void loop() {
       }
       DEBUG_PRINT("Active spool changed to: ");
       DEBUG_PRINTLN(id);
-      blank();
     }
   }
 
@@ -178,6 +168,7 @@ void loop() {
 
     // wait for long press (5s), continue if released early
     blank();
+    
     while (digitalRead(RESETPIN) == HIGH) {      
       error("HOLD",0);
       if (millis() > startPush + 2000) { // if button has been held 2s or longer
@@ -201,10 +192,12 @@ void loop() {
         while (initializing) {     
           
           // display updated capacity TODO
+          display.setTextColor(WHITE, BLACK);
           display.setCursor(10,0);
           display.println("CAPACITY:");
           display.setCursor(10,31);
-          display.println(capacity);
+          display.print(capacity);
+          display.println(" g    ");
           display.display();
                     
           // adjust capacity of new spool
@@ -213,22 +206,19 @@ void loop() {
             capacity = capacity > 0 ? capacity - 100 : 0;
             DEBUG_PRINT("Capacity ");
             DEBUG_PRINTLN(capacity);
-            delay(200);
-            blank();
+            delay(50);
           }
           if (digitalRead(NEXTPIN) == HIGH) {
             beep(2000,10);
             capacity = capacity < 9900 ? capacity + 100 : 9900;
             DEBUG_PRINT("Capacity ");
             DEBUG_PRINTLN(capacity);
-            delay(200);
-            blank();
+            delay(50);
           }
           if (digitalRead(RESETPIN) == HIGH) {
             beep(2500,10);
             beep(1000,250);
             DEBUG_PRINT("Accepted: ");
-            DEBUG_PRINTLN(capacity);
 
             // Display accepted message
             blank();
@@ -243,6 +233,7 @@ void loop() {
 
         // Record the defined capacity in the current spool's data structure
         currentSpool.capacity = capacity;
+        currentSpool.startWeight = (int) scale.get_units(20);
         
         // Record the new spool's data to eeprom
         EEPROM.put(sizeof(spool)*id, currentSpool);  // Update EEPROM with new data for this spool.          
@@ -270,21 +261,26 @@ void loop() {
   }
 
   // Display current information
+  display.setTextColor(WHITE, BLACK);
   display.setCursor(2, 0);
   display.print("SPOOL:");
-  display.print(id);
+  display.print(id+1);
+  display.setCursor(2,42);
+  display.print("                  ");
   display.setCursor(2,42);
   display.print("LEFT:");
   display.print(remaining);
   display.print("g");
+  display.display();
 }
 
 // Display an error message and optionally play an annoying notification noise
 void error(char string[], int hz) {
   // display error status TODO
+  display.setTextColor(WHITE, BLACK);
   display.setCursor(2, 20);
   display.print(string);
-  display.display();      
+  display.display();
   
   // Don't beep more often than defined
   if (millis() > lastBeep + TIMEBETWEENBEEPS && hz > 0) {
@@ -306,4 +302,5 @@ void blank() {
   // Wipe display
   display.clearDisplay();
   display.display();
+  display.setTextColor(WHITE, BLACK);
 }
